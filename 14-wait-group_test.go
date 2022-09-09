66d07a0301b2b28b4665acc17db83b0eb6139322
rasyidev <habib.rasyid11@gmail.com>
2022-09-10 05:38:28 +0700

    new file:   14-wait-group_test.go

---
+++ 14-wait-group_test.go
@@ -0,0 +1,31 @@
+package learngogoroutines
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TheRealAsynchronous(group *sync.WaitGroup) {
+	/*
+		untuk jaga jaga jika ada error pada program
+		Kalau group.Done() tidak ada, maka WaitGroup akan
+		menunggu terus, sampai deadlock karena tidak selesai selesai
+	*/
+	defer group.Done()
+
+	group.Add(1)
+	fmt.Println("Proses selesai")
+	time.Sleep(2 * time.Second)
+}
+
+func TestTheRealAsynchronous(t *testing.T) {
+	group := &sync.WaitGroup{}
+
+	for i := 0; i < 100; i++ {
+		go TheRealAsynchronous(group)
+	}
+	group.Wait()
+	fmt.Println("Selesai")
+}
