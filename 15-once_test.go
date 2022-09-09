9a07d8dcaf7d42021f4ab21d1b3d621b1c76af72
rasyidev <habib.rasyid11@gmail.com>
2022-09-10 05:54:43 +0700

    new file:   15-once_test.go

---
+++ 15-once_test.go
@@ -0,0 +1,38 @@
+package learngogoroutines
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+)
+
+var counter = 0
+
+func OnlyOnce() {
+	counter++
+	fmt.Println("Counter berjalan 1x")
+}
+
+func TestOnce(t *testing.T) {
+	group := sync.WaitGroup{}
+	once := sync.Once{}
+
+	for i := 0; i < 100; i++ {
+		group.Add(1)
+		once.Do(OnlyOnce)
+		group.Done()
+	}
+
+	group.Wait()
+	fmt.Println("Counter:", counter)
+}
+
+/*
+$ go test -v -run TestOnce
+=== RUN   TestOnce
+Counter berjalan 1x
+Counter: 1
+--- PASS: TestOnce (0.00s)
+PASS
+ok      learn-go-goroutines     0.048s
+*/
