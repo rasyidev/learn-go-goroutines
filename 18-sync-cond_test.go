3fff833d72111dee8163b388533823c5f4214e63
rasyidev <habib.rasyid11@gmail.com>
2022-09-10 09:30:21 +0700

    new file:   18-sync-cond_test.go

---
+++ 18-sync-cond_test.go
@@ -0,0 +1,37 @@
+package learngogoroutines
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+var cond = sync.NewCond(&sync.Mutex{})
+var group = sync.WaitGroup{}
+
+func WaitCondition(value int) {
+	cond.L.Lock()
+	cond.Wait()
+
+	fmt.Println("Done", value)
+	cond.L.Unlock()
+	group.Done()
+}
+
+func TestCondition(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		group.Add(1)
+		go WaitCondition(i)
+	}
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			time.Sleep(1 * time.Second)
+			cond.Signal()    // satu persatu
+			cond.Broadcast() // langsung semuanya
+		}
+	}()
+
+	group.Wait()
+}
